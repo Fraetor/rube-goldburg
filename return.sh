26b7d184433c4e7549babd3221c4a6e2453f43ea
James Frost <james@frost.cx>
2020-05-05 20:06:53 +0000

    More work

--- return.sh
+++ return.sh
@@ -2,7 +2,7 @@
 PAYLOAD=$1
 ID=176735987907559425
 
-URL='http://chaos.aksan.dev:6280/cabeiri/6de25d62f99913cb2f09103b0fb403ca'
+URL='http://chaos.aksan.dev:6280/cabeiri/d3345727237fa3217dddb40f2749523f'
 
 JSON={\"id\":$ID,\"payload\":\"$PAYLOAD\"}
 curl --location --request POST $URL \
